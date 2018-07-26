886eafe3639e317b2249ed91af3cc51fb62c8f7a
Shane <331665593@qq.com>
2018-07-26 18:17:42 +0800

    0726pm

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
 
 // 0. 如果使用模块化机制编程，导入Vue和VueRouter，要调用 Vue.use(VueRouter)
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // 1. 定义 (路由) 组件。
 // 可以从其他文件 import 进来
-import ReleaseMall from './pages/ReleaseMall.vue'
-import PersonalCenter from './pages/PersonalCenter.vue'
-import PersonalHome from './pages/PersonalHome.vue'
+import ReleaseMall from './pages/ReleaseMall.vue';
+import PersonalCenter from './pages/PersonalCenter.vue';
+import PersonalHome from './pages/PersonalHome.vue';
 
 // 2. 定义路由
 // 每个路由应该映射一个组件。 其中"component" 可以是
@@ -31,7 +30,7 @@ const routes = [
       }
     ]
   }
-]
+];
 
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
@@ -39,7 +38,7 @@ const router = new VueRouter({
   mode: 'history',
   base: __dirname,
   routes // (缩写) 相当于 routes: routes
-})
+});
 
 // 4. 创建和挂载根实例。
 // 记得要通过 router 配置参数注入路由，
